6e94d1d5d382dc793fa9ef21a874b4972fbecf39
mkaizer- <mkaizer-@student.42sp.org.br>
2022-05-31 17:08:02 +0200

    preparacao primeiro teste

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,12 @@
 /*   By: mkaizer- <mkaizer-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 19:58:33 by mkaizer-          #+#    #+#             */
-/*   Updated: 2022/05/31 03:28:57 by mkaizer-         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:06:43 by mkaizer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// The strlcat() function appends the NUL-terminated string src to the end of dst.  It will append at most size - strlen(dst) - 1 bytes, 
-// NUL-terminating the result.
-
-// For strlcat() that means the initial length of dst plus the length of src.  While this may seem somewhat confusing, it was done to make trunca‐
-// tion detection simple.
-
-// Note, however, that if strlcat() traverses size characters without finding a NUL, the length of the string is considered to be size and the
-// destination string will not be NUL-terminated (since there was no space for the NUL).  This keeps strlcat() from running off the end of a
-// string.  In practice this should not happen (as it means that either size is incorrect or that dst is not a proper “C” string).  The check ex‐
-// ists to prevent potential security problems in incorrect code.
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t			dst_len;
@@ -31,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	i = 0;
 	if (size != 0)
 	{
-		while (src[i] !='\0' && i < (size - 1))
+		while (src[i] != '\0' && i < (size - 1))
 		{
 			dst[dst_len + i] = src[i];
 			i++;
--- ft_substr.c
+++ ft_substr.c
@@ -6,20 +6,32 @@
 /*   By: mkaizer- <mkaizer-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 02:28:55 by mkaizer-          #+#    #+#             */
-/*   Updated: 2022/05/30 17:58:49 by mkaizer-         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:07:03 by mkaizer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start,size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
 	unsigned int	i;
-	unsigned int	s_len;
+	unsigned int	j;
 
-	s_len = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (len + 1);
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[1] != '\0')
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j ++;
+		}
+		i++;
+	}
+	str[j] = '\0';
+	return (str);
 }
